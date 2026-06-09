2fd0e542ca425b63286abd4f92671b6814c78490
Lingyong Sun <lingyong.sun@gmail.com>
2026-06-09 20:55:49 +0200

    fix: broken pipeline

--- src/native-test/kotlin/it/lysz210/akasha/alidrisi/ruwaat/intervals/GreetingResourceIT.kt
+++
@@ -1,6 +0,0 @@
-package it.lysz210.akasha.alidrisi.ruwaat.intervals
-
-import io.quarkus.test.junit.QuarkusIntegrationTest
-
-@QuarkusIntegrationTest
-class GreetingResourceIT : GreetingResourceTest()
